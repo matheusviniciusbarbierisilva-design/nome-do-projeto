c48bd392ddab2d5d2afff3e761460ecba62ad882
matheusviniciusbarbierisilva-design <matheusviniciusbarbierisilva@gmail.com>
2025-11-30 15:06:47 -0300

    Remove comment on automatic slider functionality
    Removed comment about automatic slider.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   alert("Bem-vindo à Ideal Store! Confira nossas promoções!");
 
-  // Slider automático
   const slides = document.querySelectorAll(".hero-slider .slide");
   let current = 0;
 
